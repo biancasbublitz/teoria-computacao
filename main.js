f9f0c1e7261f80c5068cec291779435643ffbecf
Wesley Lopes <x.wesleylopes@gmail.com>
2022-11-13 20:50:21 -0300

    refactor: get file name by command line

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const TuringMachine = require('./src/TuringMachine')
 
 function start () {
-  const turingMachine = TuringMachine('entry.txt')
+  const receivedEntryFile = process.argv.length === 3
+
+  if (!receivedEntryFile) {
+    return console.log('Entry file not found')
+  }
+
+  const [, , entryFile] = process.argv
+
+  const turingMachine = TuringMachine(entryFile)
   turingMachine.start()
 }
 
