5b825a36ccf65f3ae90aa03d5967ab54460f6362
Wesley Lopes <x.wesleylopes@gmail.com>
2022-11-13 18:52:14 -0300

    feat: read machine transictions conditions

--- main.js
+++ main.js
@@ -2,14 +2,7 @@ const TuringMachine = require('./src/TuringMachine')
 
 function start () {
   const turingMachine = TuringMachine()
-  const fileContent = turingMachine.readFile('entry.txt')
-  const fileLines = fileContent.split('\n')
-
-  const machineInfo = fileLines.slice(0, 4)
-  const machineTransitionFunctions = fileLines.filter(line => line.includes('('))
-  const machineEntry = fileLines.slice(-1)
-
-  console.log(machineTransitionFunctions)
+  turingMachine.start()
 }
 
 start()
--- src/TuringMachine.js
+++ src/TuringMachine.js
@@ -1,12 +1,47 @@
+const fs = require('fs')
+
 function TuringMachine () {
-  function readFile (fileName) {
-    const fs = require('fs')
-    const fileContent = fs.readFileSync(fileName).toString()
-    return fileContent
+  const machineHead = {
+    currentState: 0,
+    currentPosition: 0
+  }
+
+  const fileContent = fs.readFileSync('entry.txt').toString()
+  const fileLines = fileContent.split('\n')
+  const machineInfo = fileLines.slice(0, 4)
+  const machineEntry = fileLines.slice(-1).join()
+
+  function start () {
+    machineHead.currentState = machineInfo[1].split(' ')[0]
+
+    machineHead.currentPosition = machineEntry[0]
+
+    readTransitionFunctions()
+  }
+
+  function readTransitionFunctions () {
+    const machineTransitionFunctions = fileLines.filter(line => line.includes('('))
+
+    machineTransitionFunctions.forEach(transitionFunction => {
+      const [condition, result] = transitionFunction.split('=')
+      const [state, entry] = condition.replace('(', '').replace(')', '').split(',')
+
+      if (!machineInfo[1].includes(state)) {
+        throw new Error('Invalid parameter')
+      }
+
+      if (!machineInfo[3].includes(entry)) {
+        throw new Error('Invalid parameter')
+      }
+
+      // if (machineHead.currentState === state && machineEntry[machineHead.currentPosition] === entry) {
+      //   const [nextState, nextEntry, nextPosition] = result.replace('(', '').replace(')', '').split(',')
+      // }
+    })
   }
 
   return {
-    readFile
+    start
   }
 }
 
